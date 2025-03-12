8bb2873fd6f3bbd2a1483989466ef570f955f1cf
Arif Bipu <77734611+Arifbipubd@users.noreply.github.com>
2025-03-12 06:29:58 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,27 +2,27 @@
 const runtimeCaching = require("next-pwa/cache");
 const withPWA = require('next-pwa');
 const { hostname } = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`);
-const nextConfig={
+
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
   images: {
-    domains: [`${hostname}`]
+    domains: [`${hostname}`],
+    unoptimized: true, // Add the unoptimized setting here instead
   },
 }
-const pwa = process.env.NEXT_PWA_STATUS;
-const nextConfigWithPwa = withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  runtimeCaching,
-  disable: process.env.NODE_ENV === "development",
-}) (
-    nextConfig
-);
-module.exports = pwa==='1'?nextConfigWithPwa:nextConfig ;
 
-module.exports = {
-  images: {
-    unoptimized: false,
-  },
-}
+const pwa = process.env.NEXT_PWA_STATUS;
+
+// Apply PWA if enabled
+const finalConfig = pwa === '1' 
+  ? withPWA({
+      dest: "public",
+      register: true,
+      skipWaiting: true,
+      runtimeCaching,
+      disable: process.env.NODE_ENV === "development",
+    })(nextConfig)
+  : nextConfig;
+
+module.exports = finalConfig;
